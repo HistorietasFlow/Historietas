f9959eedae5e3526e8021279199f555679ba5563
HistorietasFlow <historietasflow@gmail.com>
2026-09-19 17:21:55 -0300

    refactor(perfil-autor): extrair alça visual dos menus

---
+++ app/perfil-autor/components/profile-work-action-sheet-handle.tsx
@@ -0,0 +1,5 @@
+import { workActionSheetHandleStyle } from "../styles";
+
+export function ProfileWorkActionSheetHandle() {
+  return <div style={workActionSheetHandleStyle} aria-hidden="true" />;
+}
